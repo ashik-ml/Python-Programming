75ebd24b8d59efcf4b8ee212b74553e398e23c2b
ashikrajvu115 <ashikrajvu115@gmail.com>
2023-12-11 15:47:16 +0600

    First Commit No Changes Here

---
+++ Python_Basic_1.py
@@ -0,0 +1,35 @@
+print("Hello World")
+
+#Variable Declearation , Addin two valaue with Print command
+print("Adding Two Value ..........")
+a=23
+b=20
+print("The input of a is :", a)
+print("The input of b is :", b)
+print("The sum of", a, "and", b, "is:", a+b)
+
+# Finding the area of a Circle
+print("Finding the area of a circle ..........")
+radius=8.9
+pi=3.14
+area=pi*radius*radius
+print(area)
+print("radius is : ",radius,"Pi is :",pi,"Area of this circle is : ",area)
+
+#Adding Strings
+print("Adding Two String with perfect space ...........")
+a="Learning "
+b="is fun!"
+print(a+b)
+
+# Replaceing Word by Replace function
+print("Replacing Charecter using Replace ............")
+word = "Ocygen"
+word_new=word.replace('c','x')
+print(word_new)
+
+# Substring or String slicing
+print("Find out the substring slicing..........")
+var = "String"
+sub_string_var=var[2:]
+print(sub_string_var)
